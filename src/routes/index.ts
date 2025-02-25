b2b275b44c9725f10eb05415b3330ce58266c5fa
Souvik00076 <souvikbhattacharjee00076@gmail.com>
2025-02-25 18:11:27 +0000

    Feature : stream api & rest api merged

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -14,16 +14,22 @@ import { StringOutputParser } from "@langchain/core/output_parsers";
 const router = Router();
 
 router.post("/", async (req: Request, res: Response) => {
-  const { instructions: sentence } = req.body as any;
+  const { instructions: sentence, streaming } = req.body as any;
   try {
-    const groqModel = new Groq(
+    const baseModel = new Groq(
       "mixtral-8x7b-32768",
       process.env.GROQ_API_KEY as string,
       1200,
       false,
       false,
     );
-
+    if (streaming) {
+      res.writeHead(200, {
+        "Content-Type": "text/event-stream",
+        Connection: "keep-alive",
+        "Cache-Control": "no-cache",
+      });
+    }
     const templateInputMapper = (input: {
       isArtifact: boolean;
       instruction: string;
@@ -41,7 +47,7 @@ router.post("/", async (req: Request, res: Response) => {
         }) => input.isArtifact,
         RunnableSequence.from([
           templateInputMapper,
-          artifactsPrompt.pipe(groqModel.getInstance()),
+          artifactsPrompt.pipe(baseModel.getInstance()),
         ]),
       ],
       [
@@ -52,20 +58,20 @@ router.post("/", async (req: Request, res: Response) => {
         }) => !input.isArtifact,
         RunnableSequence.from([
           templateInputMapper,
-          generalPrompt.pipe(groqModel.getInstance()),
+          generalPrompt.pipe(baseModel.getInstance()),
         ]),
       ],
       RunnableSequence.from([
         templateInputMapper,
-        artifactsPrompt.pipe(groqModel.getInstance()),
+        artifactsPrompt.pipe(baseModel.getInstance()),
       ]),
     ]);
 
-    const data = await groqModel
+    const chain = baseModel
       .setRunnable(
         new RunnableLambda({
           func: async (input: { input_data: string }) => {
-            const chain = artifactsDecisionPrompt.pipe(groqModel.getInstance());
+            const chain = artifactsDecisionPrompt.pipe(baseModel.getInstance());
             const response = await chain.invoke(input);
             return { json: response.content, instruction: input.input_data };
           },
@@ -73,12 +79,38 @@ router.post("/", async (req: Request, res: Response) => {
       )
       .setRunnable((input: { json: string; instruction: string }) => {
         const data: { isArtifact: boolean } = JSON.parse(input.json);
-        console.log(data);
         return {
           isArtifact: data.isArtifact,
           instruction: input.instruction,
         };
-      })
+      });
+    if (streaming) {
+      const data = (await chain.generateResponse({ input_data: sentence })) as {
+        isArtifact: boolean;
+        instruction: string;
+      };
+      const model = new Groq(
+        "mixtral-8x7b-32768",
+        process.env.GROQ_API_KEY as string,
+        1200,
+        true,
+        false,
+      );
+      const prompt = data.isArtifact ? artifactsPrompt : generalPrompt;
+      const streamChain = prompt
+        .pipe(model.getInstance())
+        .pipe(new StringOutputParser());
+      const stream = await streamChain.stream({
+        instruction: sentence,
+      });
+      let i = 0;
+      for await (const chunk of stream) {
+        res.write(chunk);
+      }
+      res.write(data);
+      return;
+    }
+    const data = await chain
       .setRunnable(branch)
       .setRunnable((input: any) => {
         if (!input?.content) {
@@ -87,67 +119,10 @@ router.post("/", async (req: Request, res: Response) => {
         return input.content;
       })
       .generateResponse({ input_data: sentence });
-
     res.send(data);
   } catch (error) {
     res.send(error);
   }
 });
-router.post("/stream", async (req: Request, res: Response) => {
-  const { instructions: sentence } = req.body as any;
-  try {
-    const groqModel = new Groq(
-      "mixtral-8x7b-32768",
-      process.env.GROQ_API_KEY as string,
-      1200,
-      false,
-      false,
-    );
-    res.writeHead(200, {
-      "Content-Type": "text/event-stream",
-      Connection: "keep-alive",
-      "Cache-Control": "no-cache",
-    });
-    const data = await groqModel
-      .setRunnable(
-        new RunnableLambda({
-          func: async (input: { input_data: string }) => {
-            const chain = artifactsDecisionPrompt.pipe(groqModel.getInstance());
-            const response = await chain.invoke(input);
-            return { json: response.content, instruction: input.input_data };
-          },
-        }),
-      )
-      .setRunnable((input: { json: string; instruction: string }) => {
-        const data: { isArtifact: boolean } = JSON.parse(input.json);
-        return {
-          isArtifact: data.isArtifact,
-          instruction: input.instruction,
-        };
-      })
-      .generateResponse({ input_data: sentence });
 
-    const model = new Groq(
-      "mixtral-8x7b-32768",
-      process.env.GROQ_API_KEY as string,
-      1200,
-      true,
-      false,
-    );
-    const prompt = data.isArtifact ? artifactsPrompt : generalPrompt;
-    const chain = prompt
-      .pipe(model.getInstance())
-      .pipe(new StringOutputParser());
-    const stream = await chain.stream({
-      instruction: sentence,
-    });
-    let i = 0;
-    for await (const chunk of stream) {
-      res.write(chunk);
-    }
-    res.write(data);
-  } catch (error) {
-    res.send(error);
-  }
-});
 export default router;
